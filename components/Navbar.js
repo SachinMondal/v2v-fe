4139816df24099b752b3c4a016442ec560336bb0
unknown <sachin@0307>
2025-02-08 21:35:57 +0530

    added link for smooth transition

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,12 @@
+import Link from "next/link";
 export default function Navbar() {
     return (
       <nav className="bg-gray-800 text-white p-4">
         <div className="container mx-auto flex justify-between">
           <a href="/" className="font-bold text-xl">Video Transformer</a>
           <div className="space-x-4">
-          <a href="transform" className="hover:underline">Transform</a>
-            <a href="/history" className="hover:underline">History</a>
+          <Link href="transform" className="hover:underline">Transform</Link>
+            <Link href="/history" className="hover:underline">History</Link>
           </div>
         </div>
       </nav>
