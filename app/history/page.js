a8319b99ecc36bc89ddf7d9aad6f41fd15c52640
unknown <sachin@0307>
2025-02-08 19:51:15 +0530

    added new tab in navbar and loader in the history page

--- app/history/page.js
+++ app/history/page.js
@@ -7,14 +7,17 @@ import HistoryTile from "@/components/HistoryTile";
 
 export default function History() {
   const [history, setHistory] = useState([]);
-
+  const [loading,setLoading]=useState(false);
   useEffect(() => {
     const fetchHistory = async () => {
       try {
+        setLoading(true);
         const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND}/api/history`);
         setHistory(res.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching history", error);
+        setLoading(false);
       }
     };
     fetchHistory();
@@ -26,6 +29,7 @@ export default function History() {
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Transformation History</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {loading && <div className="text-white text-4xl text-center">Loading...</div>}
           {history? history.map((vid) => (
             <HistoryTile key={vid._id} id={vid._id} sourceUrl={vid.sourceUrl? vid.sourceUrl:""} transformedUrl={vid.transformedUrl? vid.transformedUrl:""} />
           )):
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@ export default function Navbar() {
         <div className="container mx-auto flex justify-between">
           <a href="/" className="font-bold text-xl">Video Transformer</a>
           <div className="space-x-4">
+          <a href="transform" className="hover:underline">Transform</a>
             <a href="/history" className="hover:underline">History</a>
           </div>
         </div>
